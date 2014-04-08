3abf1e24e1f01077e0e5f939a33c1d1245896c49
jia1990 <446445912@qq.com>
2014-04-08 12:45:06 +0800

    增加查找菜单的方法（主要是从数据库拿）

--- oppc/src/com/oppc/Login.java
+++ oppc/src/com/oppc/Login.java
@@ -1,13 +1,22 @@
 package com.oppc;
 
+import java.util.Iterator;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.StyledEditorKit.ItalicAction;
+
+import net.sf.json.JSONArray;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.alibaba.fastjson.JSON;
+import com.oppc.user.entity.MenuEntity;
+import com.oppc.user.entity.MenuPEntity;
 import com.oppc.user.entity.UserEntity;
 import com.oppc.user.service.impl.UserServiceImpl;
 import com.oppc.util.JSONTool;
@@ -37,7 +46,23 @@ public class Login {
 		
 		System.out.println("测试动态树");
 		
-		String jsonStr = "[{'id':'1','text':'云南供电局','children':[{'id':'403351','text':'测试','children':[{'id':'403352','text':'测试小区','children':[{'id':'403360','text':'53001232','leaf':true},{'id':'403359','text':'53001231','leaf':true},{'id':'403353','text':'测试台区','leaf':true},{'id':'403355','text':'53001236','leaf':true},{'id':'403356','text':'53001237','leaf':true},{'id':'403357','text':'53001238','leaf':true},{'id':'403358','text':'53001239','leaf':true},{'id':'403361','text':'53001240','leaf':true}]}]},{'id':'402930','text':'成都军区昆明解甲园','children':[{'id':'402931','text':'解甲园','children':[{'id':'401752','text':'解甲园','leaf':true}]}]},{'id':'401741','text':'个旧市供电局','children':[{'id':'401742','text':'个旧市','children':[{'id':'402929','text':'53010001','leaf':true},{'id':'403333','text':'53010002','leaf':true},{'id':'401743','text':'丽水金湾一号变压器','leaf':true},{'id':'401751','text':'丽水金湾二号变压器','leaf':true}]}]}]}]";
+//		String jsonStr = "[{'id':'1','text':'云南供电局','children':[{'id':'403351','text':'测试','children':[{'id':'403352','text':'测试小区','children':[{'id':'403360','text':'53001232','leaf':true},{'id':'403359','text':'53001231','leaf':true},{'id':'403353','text':'测试台区','leaf':true},{'id':'403355','text':'53001236','leaf':true},{'id':'403356','text':'53001237','leaf':true},{'id':'403357','text':'53001238','leaf':true},{'id':'403358','text':'53001239','leaf':true},{'id':'403361','text':'53001240','leaf':true}]}]},{'id':'402930','text':'成都军区昆明解甲园','children':[{'id':'402931','text':'解甲园','children':[{'id':'401752','text':'解甲园','leaf':true}]}]},{'id':'401741','text':'个旧市供电局','children':[{'id':'401742','text':'个旧市','children':[{'id':'402929','text':'53010001','leaf':true},{'id':'403333','text':'53010002','leaf':true},{'id':'401743','text':'丽水金湾一号变压器','leaf':true},{'id':'401751','text':'丽水金湾二号变压器','leaf':true}]}]}]}]";
+		String jsonStr = "[{'id':46,'urlid':'','text':'菜单栏','children':[{'id':461,'urlid':'','text':'用户管理','children':[{'id':4611,'urlid':'user/Manage1.sp','text':'用户1','leaf':true},{'id':4612,'urlid':'user/Manage2.sp','text':'用户2','leaf':true}]},{'id':462,'urlid':'','text':'业务管理','children':[{'id':4621,'urlid':'business/Manage1.sp','text':'业务1','leaf':true},{'id':4622,'urlid':'business/Manage2.sp','text':'业务2','leaf':true}]}]}]";
+		/**
+		 * 将获取到的数据重新装载，便于输出json
+		 */
+		List menuList = userServiceImpl.findMenu();
+		MenuPEntity menupEntity = new MenuPEntity();
+		Iterator menuIterator = menuList.iterator();
+		
+		while(menuIterator.hasNext()){
+			MenuPEntity str = (MenuPEntity) menuIterator.next();
+			System.out.println(str.getRooId()+"==="+str.getFatherId()+"==="+str.getChildId()+"====="+str.getChildUrl());
+		}
+		//String jsonStr = "";
+		String jsonText = JSON.toJSONString(menuList, true);  
+		System.out.println(jsonText);
+		
 		
 		JSONTool.jsonWrite(response, jsonStr);
 	}

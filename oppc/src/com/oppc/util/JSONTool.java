5ca9ac08cc79c1f4367cc882574654c254b0c7b5
jia1990 <446445912@qq.com>
2014-04-08 12:47:34 +0800

    增加json的转换方法

--- oppc/src/com/oppc/util/JSONTool.java
+++ oppc/src/com/oppc/util/JSONTool.java
@@ -2,13 +2,35 @@ package com.oppc.util;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
+
+import com.alibaba.fastjson.JSON;
+
 
 
 public final class JSONTool {
 	//private static final Logger logger = LoggerFactory.getLogger(JSONTool.class);
 	
+	
+	/***
+	 * 将List对象序列化为JSON文本
+	 */
+	public static <T> String toJSONString(List<T> list) {
+		JSONArray jsonArray = JSONArray.fromObject(list);
+
+		return jsonArray.toString();
+	}
+	
+	/**
+	 * 将list转换为json格式的数据
+	 */
+	public static <T> String serialize(T object) {
+        return JSON.toJSONString(object);
+    }
 	/**
 	 * 将json数据通过printwrite返回页面
 	 * @param response
